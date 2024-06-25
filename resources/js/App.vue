acacf4676028fc259af44cd5ef30a69ccd72dbab
Patrick Her <renfu.her@gmail.com>
2024-06-25 08:49:39 +0800

    chore: Update Vue version and configure Vue 3 in Laravel 11

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Welcome to Laravel 11 with Vue 3!'
+        }
+    }
+}
+</script>
