967f55c43b613ce3ccc610f2eb48d76beeef888f
Patrick Her <renfu.her@gmail.com>
2024-06-26 14:26:20 +0800

    feat: Update target audience field in ProjectFactory.php

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -15,6 +15,10 @@ class ProjectFactory extends Factory
 
         $faker = \Faker\Factory::create('zh_TW');
 
+        $target_audience = ['公司', '工作室', '兼職上班族', '學生'];
+        // 隨機取得一組陣列中的值，ex: 公司,學生 | 工作室,兼職上班族 | 公司,學生,工作室，然後是 String
+        $target_audience = implode(', ', $faker->randomElements($target_audience, $faker->numberBetween(1, 4)));
+
         return [
             'user_id' => User::factory(),
             'name' => $faker->name,
@@ -34,7 +38,7 @@ class ProjectFactory extends Factory
             'inquiry_deadline' => $faker->date('Y-m-d'),
             'required_skills' => implode(', ', $faker->words(5)),
             'budget_range' => $faker->randomElement(['預算另議', '1~3 萬', '3~5 萬', '6~9 萬', '10~15萬', '15~20萬', '20~25', '25~30', '40~50', '50~60', '60~70', '70~80', '80~90', '90~100', '100（含）以上']),
-            'target_audience' => $faker->randomElement(['公司', '工作室', '兼職上班族', '學生']),
+            'target_audience' => $target_audience,
             'work_location' => $faker->randomElement(['皆可', '自備場所', '駐點']),
             'status' => $faker->boolean,
             'experience_years' => $faker->numberBetween(1, 20),
