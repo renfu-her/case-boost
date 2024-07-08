299736df057b22782b5ec8434f811b05876a9066
Patrick Her <renfu.her@gmail.com>
2024-07-08 17:04:08 +0800

    refactor: Update app.blade.php to include necessary JavaScript and CSS files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,20 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="">
 
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <link rel="icon" href="/logo.png" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/logo.png">
     <title>case-boost-vue</title>
     <script defer="defer" src="/js/chunk-vendors.307954dc.js"></script>
-    <script defer="defer" src="/js/app.ce0dd349.js"></script>
-    <link href="/css/chunk-vendors.8a42e1de.css" rel="stylesheet" />
-    <link href="/css/app.215d75c1.css" rel="stylesheet" />
+    <script defer="defer" src="/js/app.76a8fb2b.js"></script>
+    <link href="/css/chunk-vendors.8a42e1de.css" rel="stylesheet">
+    <link href="/css/app.971ab39f.css" rel="stylesheet">
 </head>
 
-<body>
-    <noscript><strong>We're sorry but case-boost-vue doesn't work properly without
-            JavaScript enabled. Please enable it to continue.</strong></noscript>
+<body><noscript><strong>We're sorry but case-boost-vue doesn't work properly without JavaScript enabled. Please enable
+            it to continue.</strong></noscript>
     <div id="app"></div>
 </body>
 
